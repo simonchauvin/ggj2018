ceacf53f05285712243ec0bcb2442279188ba20a
guillaume <guillaume.levieux@cnam.fr>
2018-01-28 16:53:57 +0100

    test

--- Assets/Scripts/Player/GuiguiDebugPlayer.cs
+++ Assets/Scripts/Player/GuiguiDebugPlayer.cs
@@ -11,10 +11,20 @@ public class GuiguiDebugPlayer : MonoBehaviour {
 	
 	// Update is called once per frame 
 	void Update () {
-        if(Input.GetButtonDown("Fire1")) 
+        if(Input.GetKeyDown(KeyCode.A)) 
             flock.GetComponent<NavfieldManager>().addNavfield(flock, flock.getLeader().transform.rotation, NavFieldPrimitives.dispersal,3.0f);
-        if (Input.GetButtonDown("Fire2"))
+        if (Input.GetKeyDown(KeyCode.Z))
             flock.GetComponent<NavfieldManager>().addNavfield(flock, flock.getLeader().transform.rotation, NavFieldPrimitives.gathering, 3.0f);
 
+        if (Input.GetKeyDown(KeyCode.E))
+            flock.GetComponent<NavfieldManager>().addNavfield(flock, flock.getLeader().transform.rotation, NavFieldPrimitives.ascension, 3.0f);
+        if (Input.GetKeyDown(KeyCode.R))
+            flock.GetComponent<NavfieldManager>().addNavfield(flock, flock.getLeader().transform.rotation, NavFieldPrimitives.descent, 3.0f);
+        if (Input.GetKeyDown(KeyCode.T))
+            flock.GetComponent<NavfieldManager>().addNavfield(flock, flock.getLeader().transform.rotation, NavFieldPrimitives.tube, 3.0f);
+        if (Input.GetKeyDown(KeyCode.Y))
+            flock.GetComponent<NavfieldManager>().addNavfield(flock, flock.getLeader().transform.rotation, NavFieldPrimitives.horizontal_compressor, 3.0f);
+        if (Input.GetKeyDown(KeyCode.U))
+            flock.GetComponent<NavfieldManager>().addNavfield(flock, flock.getLeader().transform.rotation, NavFieldPrimitives.vertical_compressor, 3.0f);
     }
 }
